40ef52690c195c14bb981fcbcd505ca4459f694e
MechaDragonX <theofficialrv@gmail.com>
2020-03-24 13:10:23 -0700

    Get the SumatraPDF window title using System.Diagnostics itself

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.IO;
 
 namespace Chilong
 {
@@ -8,34 +7,8 @@ namespace Chilong
     {
         static void Main(string[] args)
         {
-            string solutionDir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
-            try
-            {
-                var process = new Process
-                {
-                    StartInfo = new ProcessStartInfo
-                    {
-                        FileName = Path.Combine(solutionDir, "win-info/win-info-win32.exe"),
-                        UseShellExecute = false,
-                        RedirectStandardOutput = true,
-                        CreateNoWindow = true
-                    }
-                };
-
-                process.Start();
-
-                while(!process.StandardOutput.EndOfStream)
-                {
-                    var line = process.StandardOutput.ReadLine();
-                    Console.WriteLine(line);
-                }
-
-                process.WaitForExit();
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Process sumatra = Process.GetProcessesByName("SumatraPDF")[0];
+            Console.WriteLine("Id: {0}, MainWindowTitle: {1}", sumatra.Id, sumatra.MainWindowTitle);
         }
     }
 }
