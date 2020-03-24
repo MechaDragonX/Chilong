3a4674082531236a12e8083d2b1b16a71d5dfd7b
MechaDragonX <theofficialrv@gmail.com>
2020-03-24 12:34:12 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chilong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
