c8d825f388bf4a37bc39fb01b5cc1ea3fe946b3f
MechaDragonX <theofficialrv@gmail.com>
2020-03-24 12:54:34 -0700

    Get active window metadata using https://github.com/arciisine/win-info

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace Chilong
 {
@@ -6,7 +8,34 @@ namespace Chilong
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string solutionDir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            try
+            {
+                var process = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = Path.Combine(solutionDir, "win-info/win-info-win32.exe"),
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
+                    }
+                };
+
+                process.Start();
+
+                while(!process.StandardOutput.EndOfStream)
+                {
+                    var line = process.StandardOutput.ReadLine();
+                    Console.WriteLine(line);
+                }
+
+                process.WaitForExit();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
