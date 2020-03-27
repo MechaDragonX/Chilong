35b9603b0868f1d27712c33a1683275be8a705bd
MechaDragonX <theofficialrv@gmail.com>
2020-03-27 16:08:47 -0700

    Fix the start time issue!

--- Program.cs
+++ Program.cs
@@ -9,21 +9,25 @@ namespace Chilong
     {
         static void Main(string[] args)
         {
-            if(Process.GetProcesses().ToList().Where(x => x.ProcessName.StartsWith("SumatraPDF")).Count() == 0)
+            if(Process.GetProcesses().Where(x => x.ProcessName.StartsWith("SumatraPDF")).Count() == 0)
             {
                 Console.WriteLine("SumatraPDF was not found! Is it open?");
                 return;
             }
 
-            Process sumatra = Process.GetProcesses().ToList().Where(x => x.ProcessName.StartsWith("SumatraPDF")).ToList()[0];
+            Process sumatra = Process.GetProcesses().Where(x => x.ProcessName.StartsWith("SumatraPDF")).ToList()[0];
             using(DiscordRpcClient client = new DiscordRpcClient("692113594623721514"))
             {
-                if(!client.Initialize())
+                try
                 {
-                    Console.WriteLine("Connection to client was not successful!");
+                    client.Initialize();
+                    Console.WriteLine("Successfully connected to client!");
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine($"Connection to client was not successful!\nERROR: {e.Message}");
                     return;
                 }
-                Console.WriteLine("Successfully connected to client!");
 
                 try
                 {
@@ -31,7 +35,7 @@ namespace Chilong
                     {
                         Details = sumatra.MainWindowTitle.TrimEnd(" - SumatraPDF".ToCharArray()),
                         State = "State Placeholder",
-                        Timestamps = new Timestamps(DateTime.Now),
+                        Timestamps = new Timestamps(DateTime.UtcNow),
                         Assets = new Assets()
                         {
                             LargeImageKey = "sumatra",
@@ -42,15 +46,16 @@ namespace Chilong
                 }
                 catch(Exception e)
                 {
-                    Console.WriteLine("Setting presence was not successful!");
+                    Console.WriteLine($"Setting presence was not successful!\nERROR: {e.Message}");
                     return;
                 }
                 
                 while(true)
                 {
-                    if(Process.GetProcesses().ToList().Where(x => x.ProcessName.StartsWith("SumatraPDF")).Count() == 0)
+                    if(Process.GetProcesses().Where(x => x.ProcessName.StartsWith("SumatraPDF")).Count() == 0)
                     {
                         client.ClearPresence();
+                        Console.WriteLine("Thanks for using Chilong!");
                         return;
                     }
                 }
